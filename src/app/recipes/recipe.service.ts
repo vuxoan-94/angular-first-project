0a10acca7aadfcdf7d87915fbff1927ca3de3ae8
Nguyễn Hoàng Vũ <hoangvu.ng2@samsung.com>
2017-12-21 17:23:34 +0700

    21/12/2017

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import {Recipe} from "./recipe.model";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Meat Powder Recipe Food Dishes Pork',
+      'Delecious dish!',
+      'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg',
+      [
+        new Ingredient('Pork', 1),
+        new Ingredient('Powder', 1)
+      ]),
+    new Recipe(
+      'Chocolate Crinkles',
+      'This is totally awesome',
+      'http://images.media-allrecipes.com/userphotos/720x405/430444.jpg',
+      [
+        new Ingredient('Cocoa powder', 1),
+        new Ingredient('Egg', 4)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipe() {
+    return this.recipes.slice(); //get a copy of recipes array instead of reference to it
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,24 @@
+import {Ingredient} from "../shared/ingredient.model";
+import {EventEmitter} from "@angular/core";
+
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients); // ... is to conver array to a list of elements which is supported by push method
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
